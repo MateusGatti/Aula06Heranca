2b1303fc243ebee5bc076f0289921819e328584b
Mateus Mello <mt.gatti@hotmail.com>
2020-05-29 14:53:40 -0300

    Projeto Funcionando

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Aula06Heranca
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            CPF cpf = new CPF();
+            cpf.nome = "Mateus";
+            Console.WriteLine( cpf.Saudar() );
+
+            cpf.cpf = "123.456.789.01";
+            Console.WriteLine( cpf.ValidarCpf () );
+
+            CNPJ cnpj = new CNPJ();
+            cnpj.nome = "Gatti";
+            Console.WriteLine( cnpj.Saudar() );
+
+            cnpj.cnpj = "123.456.789.01";
+            Console.WriteLine( cnpj.ValidarCNPJ() );
+
+            
+    
+        }
+    }
+}
